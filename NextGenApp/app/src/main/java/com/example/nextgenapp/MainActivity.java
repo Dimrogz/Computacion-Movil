e7e5884d09b6b53301db08542e80094f93312a7d
Dimrogz <dimrogz1213@gmail.com>
2023-05-15 12:13:10 -0500

    Update
    Update

--- NextGenApp/app/src/main/java/com/example/nextgenapp/MainActivity.java
+++ NextGenApp/app/src/main/java/com/example/nextgenapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.nextgenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Entrar;
+    Button imgButtonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Entrar=(Button)findViewById(R.id.Entrar);
+        Entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Paginaprin.class);
+                startActivity(i);
+
+            }
+        });
+
+
     }
 }
--- NextGenApp/app/src/main/java/com/example/nextgenapp/Registro.java
+++ NextGenApp/app/src/main/java/com/example/nextgenapp/Registro.java
@@ -2,13 +2,20 @@ package com.example.nextgenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Registro extends AppCompatActivity {
+
+    ImageButton imgButtonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
     }
+
 }
